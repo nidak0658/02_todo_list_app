b91c519cd8f796f46c0084a9ba0427515cd2136b
Nida Khan <nidak0658@gmail.com>
2024-05-18 01:17:19 +0000

    first

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="mainBg h-[calc(100vh-90px)]
+    flex
+    flex-col
+    justify-center
+    items-center
+    gap-10
+    ">
+      <h1 className=" text-2xl lg:text-6xl font-bold">Welcome to IB Coding School</h1>
+
+      <h2 className="lg:text-3xl font-bold">Here you can make your ToDo List</h2>
+
+      <Link className="
+      bg-blue-800
+      p-1 rounded
+      text-white
+      text-2xl
+      px-4
+      hover:bg-blue-900
+      " href={'/todo'}>Get Started</Link>
+    </div>
+  );
+}
